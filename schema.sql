0a2f6b26515602fffa504a39a95e7a1fff9f204e
Esteban <cepalacios@outlook.com>
2023-11-06 18:55:20 -0500

    Update schema.sql
    stylize with tab so it looks better

--- schema.sql
+++ schema.sql
@@ -1,12 +1,12 @@
-/* Database schema to keep the structure of entire database. */
+/* Database schema to keep the structure of the entire database. */
 
 
 CREATE TABLE animals(
-  id				INT GENERATED ALWAYS AS IDENTITY,
-  name			VARCHAR(100) NOT NULL,
+  id				      INT GENERATED ALWAYS AS IDENTITY,
+  name			      VARCHAR(100) NOT NULL,
   date_of_birth		DATE,
   escape_attempts	INT NOT NULL,
-  neutered 			BOOLEAN NOT NULL,
-  weight_kg			DECIMAL NOT NULL,
+  neutered 			  BOOLEAN NOT NULL,
+  weight_kg			  DECIMAL NOT NULL,
   PRIMARY KEY(id)
 );
