359551999dd381934933fdf7c44dc671f393d221
efmomoh <efmomoh25@gmail.com>
2023-11-14 01:31:03 +0000

    Modifies the queries.sql file content

--- queries.sql
+++ queries.sql
@@ -209,3 +209,23 @@ FROM (
 JOIN animals a ON subquery.species_id = a.species_id
 JOIN species s ON a.species_id = s.id
 GROUP BY s.name;
+
+-- ENSSAH queries:
+-- This will add 3.594.280 visits considering you have 10 animals, 4 vets, and it will use around ~87.000 timestamps (~4min approx.)
+INSERT INTO visits (animal_id, vet_id, date_of_visit)
+SELECT animal_ids.id, vets_ids.id, visit_timestamp
+FROM
+    (SELECT id FROM animals) AS animal_ids,
+    (SELECT id FROM vets) AS vets_ids,
+    generate_series('1980-01-01'::timestamp, '2021-01-01', '4 hours') AS visit_timestamp
+ON CONFLICT (vet_id, animal_id, date_of_visit) DO NOTHING;
+
+-- This will add 2.500.000 owners with full_name = 'Owner <X>' and email = 'owner_<X>@email.com' (~2min approx.)
+insert into owners (full_name, email) select 'Owner ' || generate_series(1,2500000), 'owner_' || generate_series(1,2500000) || '@mail.com';
+
+
+EXPLAIN ANALYZE SELECT COUNT(*) FROM visits WHERE animal_id = 4;
+
+SELECT COUNT(*) FROM visits where animal_id = 4;
+SELECT * FROM visits where vet_id = 2;
+SELECT * FROM owners where email = 'owner_18327@mail.com';
